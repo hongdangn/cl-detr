891de9dbe75712e61bd06eafb5ad44af6acf1b34
hongdangn <ddangdangnh@gmail.com>
2024-09-10 15:11:25 +0700

    fix setup.py

--- models/ops/setup.py
+++ models/ops/setup.py
@@ -25,17 +25,17 @@ def get_extensions():
     extra_compile_args = {"cxx": []}
     define_macros = []
 
-    if torch.cuda.is_available() and CUDA_HOME is not None:
-        extension = CUDAExtension
-        sources += source_cuda
-        define_macros += [("WITH_CUDA", None)]
-        extra_compile_args["nvcc"] = [
-            "-DCUDA_HAS_FP16=1",
-            "-D__CUDA_NO_HALF_OPERATORS__",
-            "-D__CUDA_NO_HALF_CONVERSIONS__",
-            "-D__CUDA_NO_HALF2_OPERATORS__",
-        ]
-    else:
+    # if torch.cuda.is_available() and CUDA_HOME is not None:
+    extension = CUDAExtension
+    sources += source_cuda
+    define_macros += [("WITH_CUDA", None)]
+    extra_compile_args["nvcc"] = [
+        "-DCUDA_HAS_FP16=1",
+        "-D__CUDA_NO_HALF_OPERATORS__",
+        "-D__CUDA_NO_HALF_CONVERSIONS__",
+        "-D__CUDA_NO_HALF2_OPERATORS__",
+    ]
+    # else:
         raise NotImplementedError('Cuda is not availabel')
 
     sources = [os.path.join(extensions_dir, s) for s in sources]
