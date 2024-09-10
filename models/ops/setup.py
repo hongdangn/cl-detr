98f2326f43a468c898564d07ebc54c179bd36c22
hongdangn <ddangdangnh@gmail.com>
2024-09-10 15:14:22 +0700

    fix setup.py

--- models/ops/setup.py
+++ models/ops/setup.py
@@ -36,7 +36,7 @@ def get_extensions():
         "-D__CUDA_NO_HALF2_OPERATORS__",
     ]
     # else:
-        raise NotImplementedError('Cuda is not availabel')
+        # raise NotImplementedError('Cuda is not availabel')
 
     sources = [os.path.join(extensions_dir, s) for s in sources]
     include_dirs = [extensions_dir]
